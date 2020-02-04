831dd88683de2c53f493a02041c96063cb3d8207
SinshiSmith <yorfnd@gmail.com>
2020-02-04 13:34:00 +0200

    /index: Add example Object Preview

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FieldRow from "../components/FieldRow";
 import Button from "../components/Button";
+
+const generator = rows =>
+  JSON.stringify(
+    Object.values(rows).reduce(
+      (current, { name, fields }) => ({
+        ...current,
+        [name]: fields[Math.floor(Math.random() * fields.length)]
+      }),
+      {}
+    ),
+    null,
+    2
+  );
 
 const index = () => {
   const [rows, setRows] = useState({
     1: { name: "Field 1", fields: [] }
   });
+  const [example, setExample] = useState("");
+  useEffect(() => {
+    setExample(generator(rows));
+  }, [rows, setExample]);
   return (
     <div>
       <h1 css={{ fontFamily: "'Great Vibes', cursive" }}>RandomDB</h1>
@@ -39,10 +56,16 @@ const index = () => {
       <h2 css={{ textTransform: "capitalize" }}>
         possible objects count:&nbsp;
         {Object.values(rows).reduce(
-          (total, field) => total * field.fields.length,
+          (total, field) => total * (field.fields.length || 1),
           1
         )}
       </h2>
+      <Button onClick={() => setExample(generator(rows))}>
+        Example Object
+      </Button>
+      <div>
+        <pre>{example}</pre>
+      </div>
     </div>
   );
 };
